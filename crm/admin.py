fc6713df8f3eef47043d9bb0c96b73238099aa1e
Christian Angermeier <ca@angermeier.net>
2026-05-31 20:42:38 +0000

    asd

--- crm/admin.py
+++ crm/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from crm.models import Contact, EducationRequest
+
 # Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'status', 'assigned_to')
+    list_filter = ('status', 'source')
+    search_fields = ('first_name', 'last_name', 'email', 'company')
+    ordering = ('-id',)
+
+@admin.register(EducationRequest)
+class EducationRequestAdmin(admin.ModelAdmin):
+    list_display = ('institution_name', 'institution_type', 'email', 'website', 'user_count', 'status')
+    list_filter = ('institution_type', 'status','user_count')
+    search_fields = ('institution_name', 'email', 'website', 'status')
+    ordering = ('-id',)
--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from .models import Subscriber, Campaign
+
 # Register your models here.
+@admin.register(Subscriber)
+class SubscriberAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'status', 'source')
+    list_filter = ('status', 'source')
+    search_fields = ('first_name', 'last_name', 'email')
+    ordering = ('-id',)
+
+@admin.register(Campaign)
+class CampaignAdmin(admin.ModelAdmin):
+    list_display = ('name', 'subject', 'email', 'segment', 'status')
+    list_filter = ('segment', 'status')
+    search_fields = ('name', 'subject', 'email', 'segment', 'status')
+    ordering = ('-id',)
